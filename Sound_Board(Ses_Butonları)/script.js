15c4f1232d04d4aec685eba0f46819f444f58e87
turgutergovan <95032560+turgutergovan@users.noreply.github.com>
2022-04-05 00:26:57 +0300

    Day9

---
+++ Sound_Board(Ses_Butonları)/script.js
@@ -0,0 +1,25 @@
+const sounds = ['Ambience', 'Ambience2','Bird','DrippingWater','Fire','GentleRainfall','HeavyRain','Jungle','Nature','Night','Rain','RainAndThunder','Thunder']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
